2877b68ad567704ea02072955efd8251edb73d9c
Leon <leoding2009@gmail.com>
2026-04-13 17:47:28 +1200

    Refactor code structure for improved readability and maintainability

---
+++ public/js/my.js
@@ -0,0 +1,10 @@
+/* Logo */
+function logotaichiRun(){
+    var el=document.getElementById("logotaichi");
+    el.setAttribute("class","logoisrunning");
+}
+
+function logotaichiPause(){
+    var el=document.getElementById("logotaichi");
+    el.setAttribute("class","logoTaichi");
+}
